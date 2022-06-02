fa045d6c5191d33dddfa8614f11ea1b773356456
Darren <darren2097@gmail.com>
2022-06-02 07:45:17 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char (line[]) = "Programming is like building a multilingual puzzle\n";
+	puts(line);
+	return (0);
+}
