cee80b5d5e40ea350a85872a6f80ff7aa9697be6
Darren <darren2097@gmail.com>
2022-06-02 07:49:20 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char (line[]) = "\"Programming is like building a multilingual puzzle\n";
+	char (line[]) = "\"Programming is like building a multilingual puzzle";
 	puts(line);
 	return (0);
 }
