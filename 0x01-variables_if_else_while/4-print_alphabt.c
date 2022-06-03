dfc2fdf1c52a56c13cf88fd8daedf3d126cc2ed4
Darren <darren2097@gmail.com>
2022-06-03 10:11:49 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet without q and e
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int count = 97;
+
+	while (count <= 122)
+	{
+		if (count == 101)
+			count = 102;
+		END;
+
+		if (count == 113)
+			count = 114;
+		END;
+
+		char letter = count;
+
+		putchar(letter);
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
