07041ff385baa9dc0c2c41909131df2a10ce4715
Darren <darren2097@gmail.com>
2022-06-03 07:10:57 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char count;
+
+	count = 'a';
+
+	while count <= 'z'
+	{
+		putchar("%c", count);
+		count++;
+	}
+	return (0);
+}
