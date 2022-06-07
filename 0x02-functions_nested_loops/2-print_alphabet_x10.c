0b62a061fbac64a858d0ca9c739551f91a85d335
Darren <darren2097@gmail.com>
2022-06-07 11:35:44 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int count1, count2;
+
+	count1 = 0;
+
+	while (count1 <= 10)
+	{
+		count2 = 97;
+		while (count2 < 123)
+		{
+			char c = count2;
+			_putchar(c);
+			count2++;
+		}
+		_putchar('\n');
+		count1++;
+	}
+	return;
+}	
