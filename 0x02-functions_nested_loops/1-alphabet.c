a14410fe0bc1665633c6f2b0e170ce376f9f51ba
Darren <darren2097@gmail.com>
2022-06-07 07:19:25 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int count = 97;
+
+	while (count < 123)
+	{
+		char letter = count;
+		putchar(letter);
+		count++;
+	}
+
+	return (0);
