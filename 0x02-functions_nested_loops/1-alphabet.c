8237b7654bb484c0f64a07216f55ab583430e99d
Darren <darren2097@gmail.com>
2022-06-07 08:35:09 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,3 +11,17 @@ int main(void)
 	void print_alphabet(void);
 	return (0);
 }
+
+void print_alphabet(void)
+{
+	int count = 97;
+
+	while (count < 123)
+	{
+		char letter = count;
+
+		putchar(letter);
+		count++;
+	}
+	return;
+}
