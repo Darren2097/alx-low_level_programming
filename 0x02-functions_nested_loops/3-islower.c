b5a8ef256ef6964bfda677be36b015c5c7c87a2a
Darren <darren2097@gmail.com>
2022-06-07 13:12:22 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,16 @@
  * Return: returns 1 if lowercase and 0 if otherwise
  */
 
-int _islower(int c)
+int _islower(void)
 {
+	int c;
+
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 	else
-	{	
+	{
 		return (0);
 	}
 }
