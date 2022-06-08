09551bc07c9e9ea90f6f6993fef4149692001c24
Darren <darren2097@gmail.com>
2022-06-08 05:02:02 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,6 +23,7 @@ void print_to_98(int n)
 
 			n++;
 		}
+	}
 	else if (n > 98)
 		{
 		while (n != 98)
@@ -37,13 +38,11 @@ void print_to_98(int n)
 
 			n++;
 		}
+		}
 		else
 		{
 		_putchar(n);
 		_putchar('\n');
 		}
-		}
-	
-	}
 	}
 }
