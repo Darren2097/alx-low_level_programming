5dfe9aa29e9364977962b46a7eea8ba66bbfb51a
Darren <darren2097@gmail.com>
2022-06-08 10:58:32 -0700

    task 102

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the sum of the even-valued terms in the fibonacci sequence
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int a, b, sum, totsum;
+	
+	a = 0;
+	b = 1;
+	sum = 0;
+	totsum = 0;
+
+	while (sum < 4000000)
+	{
+		sum = a + b;
+
+		if (sum % 2 == 0)
+		{
+			totsum += sum;
+		}
+
+		a = b;
+		b = sum;
+	}
+
+	printf("%li\n", totsum);
+	return (0);
+}
