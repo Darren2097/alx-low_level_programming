82cb0258184a663b882f87db3d64f195e84271b8
Darren <darren2097@gmail.com>
2022-06-08 03:14:25 -0700

    task 10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers to 98
+ * @n: n is an ASCII character
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n != 98)
+		{
+			_putchar(n + '0');
+
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+
+			n++;
+		}
+	else if (n > 98)
+		while (n != 98)
+		{
+			_putchar(n + '0');
+
+			if (n != 98)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+
+			n++;
+		}
+	else
+		_putchar(n);
+		_putchar('\n');
+	}
+}
