87c5bfeeac0e45ca519b6d9760b6c4a1a3061863
Darren <darren2097@gmail.com>
2022-06-09 06:05:53 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - Prints numbers from 0 to 9
  *
- * Return: Always 0 (success)
+ * Return: void
  */
 
 void print_numbers(void)
@@ -13,7 +13,7 @@ void print_numbers(void)
 	count = 0;
 	while (count <= 9)
 	{
-		_putchar(count + '0');
+		_putchar(count);
 		count++;
 	}
 	_putchar('\n');
