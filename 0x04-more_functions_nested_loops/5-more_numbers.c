aa8c93ecf3345259feced68a1080b02be47092c4
Darren <darren2097@gmail.com>
2022-06-09 06:40:16 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j, first, second;
+
+	i = 0;
+	while (i <= 10)
+	{
+		first = 0;
+		while (first < 2)
+		{
+			second = 0;
+			while (first < 2 && second <= 5)
+			{
+				_putchar(second + '0');
+				second++;
+			}
+			_putchar(first + '0');
+			first++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
