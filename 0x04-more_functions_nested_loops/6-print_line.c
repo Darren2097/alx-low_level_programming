a0f327e768fe1efae50c2aa9343a9c70f5c6f22a
Darren <darren2097@gmail.com>
2022-06-09 07:32:20 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,17 +12,10 @@ void print_line(int n)
 	int numTimes;
 
 	numtimes = 0;
-	if (n > 0)
+
+	while (n > numtimes)
 	{
-		while (n > numtimes)
-		{
-			_putchar('_');
-			numtimes++;
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
+		_putchar('_');
+		numtimes++;
 	}
 }
