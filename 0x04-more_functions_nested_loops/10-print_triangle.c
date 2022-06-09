e0c14b97abb81ebd895aba6046860f94fafb9d34
Darren <darren2097@gmail.com>
2022-06-09 12:19:27 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,22 +11,21 @@ void print_triangle(int size)
 {
 	int r, hash, spaces;
 
-	r = 0;
-
 	if (size > 0)
 	{
-		while (r < size)
+		for (r = 0; r < size; r++)
 		{
 			for (spaces = size - 1; spaces > r; spaces--)
 			{
 				_putchar(' ');
 			}
+
 			for (hash = 0; hash < r + 1; hash++)
 			{
 				_putchar('#');
 			}
-			_putchar('n');
-			r++;
+
+			_putchar('\n');
 		}
 	}
 	else
