c3df8c29f547bffcbcea4581e697df62972b6f19
Darren <darren2097@gmail.com>
2022-06-09 07:26:50 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line across the terminal
+ * @n: n is an ASCII character
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int numTimes;
+
+	numtimes = 0;
+	if (n > 0)
+	{
+		while (n >= numtimes)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
