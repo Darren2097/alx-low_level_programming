e1033186e0b9d4125b75e57ee5cd49a08805de69
Darren <darren2097@gmail.com>
2022-06-13 09:48:39 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,6 +14,14 @@ void print_array(int *a, int n)
 	int count, last;
 
 	last = n - 1;
+
+	if (n < 0 || n == 0)
+	{
+		n = 0;
+	}
+	else
+	{
+
 	for (count = 0; count < n; count++)
 	{
 		if (count == last)
@@ -24,5 +32,6 @@ void print_array(int *a, int n)
 		{
 			printf("%d, ", a[count]);
 		}
+	}
 	}
 }
