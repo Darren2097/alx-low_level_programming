0d1d8a3a86f83df28f5404408d2ef490a6f5bd9c
Darren <darren2097@gmail.com>
2022-06-13 11:59:56 -0700

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to integer
+ * @s: string variable
+ *
+ * Return: returns 0 if no integer in string
+ */
+
+int _atoi(char *s)
+{
+	int val;
+
+	val = atoi(s);
+
+	if (val == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (val);
+	}
+}
