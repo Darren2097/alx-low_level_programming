575f853c1240d48736d7b546c442fc2f0be9d473
Darren <darren2097@gmail.com>
2022-06-13 07:46:52 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: string variable
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int count, len;
+
+	len = strlen(s) - 1;
+
+	for (count = len, count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+
+	_putchar('\n');
+}
