f9b5ad7f64e1be680a57feaa2a2f31310f384f4f
Darren <darren2097@gmail.com>
2022-06-13 08:41:37 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string variable
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int count;
+
+	count = 0;
+	while (count < strlen(str) + 1)
+	{
+		_putchar(str[count]);
+		count = count + 2;
+	}
+
+	_putchar('\n');
+}
