d96abb3ef200ef42e0921165dc4ba83c8aca5aed
Darren <darren2097@gmail.com>
2022-06-16 09:44:21 -0700

    task 103

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result of the numbers added
+ * @size_r: buffer size
+ *
+ * Return: returns variable r and 0 if the result can't be stored in r
+ */
+
+char *infinte_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	r = n1 + n2;
+}
