f1fccb42374333da537e08ae6fb236a1ff41de4a
Darren <darren2097@gmail.com>
2022-06-16 08:01:51 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *rot13 - replaces the string letters with the letter 13 after that letter
+ * @str: string variable
+ *
+ * Return: returns the result string
+ */
+
+char *rot13(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if ((str[count] >= 'a' && str[count] <= 'z') ||
+				(str[count] >= 'A' && str[count] <= 'Z'))
+		{
+			str[count] = str[count] + 13;
+		}
+	}
+	return (str);
+}				
