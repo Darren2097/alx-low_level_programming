35d0e171ac000295814cb69441e20226e7a6eca2
Darren <darren2097@gmail.com>
2022-06-16 08:11:37 -0700

    task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: n is an integer
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	_putchar(n + '0');
+	_putchar('\n');
+}
