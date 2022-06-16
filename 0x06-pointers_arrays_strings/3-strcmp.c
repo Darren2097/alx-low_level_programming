f9f9eaeb06beb2b1ade079dcf477e09890d617b7
Darren <darren2097@gmail.com>
2022-06-16 02:26:17 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string variable
+ * @s2: string variable
+ *
+ * Return: returns 0 if the same, less than 0 if s1<s2 and more than 0 if s2>s1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
