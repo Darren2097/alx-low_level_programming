d21b7b3f9fc2f6283f60a9001b4eeb08b4ba697c
Darren <darren2097@gmail.com>
2022-06-22 05:41:47 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _print_rev_recursion - prints a string in reverse
@@ -10,6 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	s = strrev(s);
-	_putchar(s);
+	if (s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s);
+	}
 }
