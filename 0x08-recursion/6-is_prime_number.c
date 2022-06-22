8d3773bc42a7d13180b308847741baf8e18619a2
Darren <darren2097@gmail.com>
2022-06-22 07:10:16 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,6 +9,11 @@
 
 int is_prime_number(int n)
 {
+	if (n == 1)
+	{
+		return (0);
+	}
+
 	if (n > 0)
 	{
 		if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
@@ -19,10 +24,6 @@ int is_prime_number(int n)
 		{
 			return (1);
 		}
-	}
-	else if (n == 1)
-	{
-		return (0);
 	}
 	else
 		return (0);
