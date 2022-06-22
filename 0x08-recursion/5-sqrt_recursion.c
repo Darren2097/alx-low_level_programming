b9f6625e8418028ddc90a56d832bafa6b98f0b44
Darren <darren2097@gmail.com>
2022-06-22 09:40:17 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 
 /**
  * _sqrt_recursion - finds the natural square root of a number
@@ -11,14 +10,32 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n > 0)
-	{
-		return (sqrt(n));
-	}
+	int i;
+
+	i = 2;
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (check_sqrt(n, i));
+}
+
+/**
+ * check_sqrt - checks if number given is a perfect square and calculates
+ * its square root
+ * @num: num is an integer
+ * @count: count is an integer
+ *
+ * Return: returns the square root or -1 if its not a perfect square
+ */
+
+int check_sqrt(int num, int count)
+{
+	if (count * count == num)
+		return (count);
+	else if (count < num)
+		count++;
 	else
 		return (-1);
 }
