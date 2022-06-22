7fbdeee9611d340c97f1dcba22db2e180fe8e7d2
Darren <darren2097@gmail.com>
2022-06-22 05:50:07 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <strlen>
+
+/**
+ * _strlen_recursion - gets the length of the string
+ * @s: string variable
+ *
+ * Return: returns the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
