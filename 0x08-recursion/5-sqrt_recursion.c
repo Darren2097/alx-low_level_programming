98f26330c6ed1cf4a26d0b8b2e0a49fbd96aaf4f
Darren <darren2097@gmail.com>
2022-06-22 07:12:52 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,11 +11,7 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		return (0);
 	}
@@ -23,4 +19,6 @@ int _sqrt_recursion(int n)
 	{
 		return (sqrt(n));
 	}
+	else
+		return (-1);
 }
