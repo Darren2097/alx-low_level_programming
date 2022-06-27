31438e0c55c5ec4742106e09e09949b019044f3c
Darren <darren2097@gmail.com>
2022-06-27 07:27:21 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,35 +16,33 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
-	else
+
+	length1 = 0;
+	while (*(s1 + length1) != '\0')
 	{
-		length1 = 0;
-		while (*(s1 + length1) != '\0')
-		{
-			length1++;
-		}
-		length2 = 0;
-		while (*(s2 + length2) != '\0')
-		{
-			length2++;
-		}
-		lengthtot = length1 + length2;
-
-		ar = (char *) malloc(lengthtot * sizeof(char));
-
-		if (ar == NULL)
-			return (NULL);
-
-		for (count = 0; count < length1; count++)
-		{
-			*(ar + count) = s1[count];
-		}
-		for (count = 0; count < length2; count++)
-		{
-			*ar[(count + length1)] = s2[count];
-		}
-		return (ar);
+		length1++;
 	}
+	length2 = 0;
+	while (*(s2 + length2) != '\0')
+	{
+		length2++;
+	}
+	lengthtot = length1 + length2;
+
+	ar = (char *) malloc(lengthtot * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (count = 0; count < length1; count++)
+	{
+		*(ar + count) = s1[count];
+	}
+	for (count = 0; count < length2; count++)
+	{
+		ar[count + length1] = s2[count];
+	}
+	return (ar);
 }
