8b5f3534fd69ce21541bdc27483ceaba53f889ac
Darren <darren2097@gmail.com>
2022-06-27 06:34:30 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - returns a pointer which contains a copy of the string given
+ * @str: string variable
+ *
+ * Return: returns NULL if str = NULL, else returns the string
+ */
+
+char *_strdup(char *str)
+{
+	char s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		s = strdup(str);
+		return (s);
+	}
+}
