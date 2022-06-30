29859532ae5275e9df66e92da0b09ed5b63261f9
Darren <darren2097@gmail.com>
2022-06-30 07:04:33 -0700

    task 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: returns the answer or 98 if number of arguments is incorrect
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc = 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
