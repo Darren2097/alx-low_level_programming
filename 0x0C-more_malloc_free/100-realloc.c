d932437b8384466b65c96b50b51aaf61bae553e6
Darren <darren2097@gmail.com>
2022-06-30 06:37:28 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,23 +16,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-	else if (new_size < old_size)
+	
+	if (new_size == 0 && ptr != NULL)
 	{
-		if (new_size == 0 && ptr != NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
+		free(ptr);
+		return (NULL);
+	}
 
-		if (ptr == NULL)
-		{
-			ptr = malloc(new_size);
-			return (ptr);
-		}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
 
 	free(ptr);
 
 	ptr = realloc(ptr, new_size);
 	return (ptr);
-	}
 }
