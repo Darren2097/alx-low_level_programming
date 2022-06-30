9e5890b22add09b7d9fff49280c1b72baa8f2862
Darren <darren2097@gmail.com>
2022-06-30 06:35:06 -0700

    task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - reallocates a memory block
+ * @ptr: pointer
+ * @old_size: size of ptr
+ * @new_size: new size of new memory block
+ *
+ * Return: returns pointer with new size, else return NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else if (new_size < old_size)
+	{
+		if (new_size == 0 && ptr != NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+
+		if (ptr == NULL)
+		{
+			ptr = malloc(new_size);
+			return (ptr);
+		}
+
+	free(ptr);
+
+	ptr = realloc(ptr, new_size);
+	return (ptr);
+	}
+}
