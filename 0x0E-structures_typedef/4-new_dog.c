aca3bd75090b634aff63fae3cbf74d3045fd6194
Darren <darren2097@gmail.com>
2022-07-04 10:30:04 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
 	char *dogname, *dogowner;
-	/*int count, length;*/
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
@@ -24,25 +23,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dogname = strdup(name);
 	dogowner = strdup(owner);
+
 	if (dogname == NULL || dogowner == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
-	/**length = 0;
-	while (*(dogname + length) != '\0')
-	{
-		length++;
-	}
-	dname = malloc(length * sizeof(char) + 1);
-	if (dname == NULL)
-		return (NULL);
 
-	for (count = 0; count < length; count++)
-	{
-		*(dname + count) = *(dogname + count);
-	}
-*/
 	newdog->name = dogname;
 	newdog->age = age;
 	newdog->owner = dogowner;
