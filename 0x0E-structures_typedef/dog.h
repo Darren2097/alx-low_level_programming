e59dd1b7ffcce873ccef5f27a84c3a8333e7a328
Darren <darren2097@gmail.com>
2022-07-04 07:58:41 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define new type struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dogs owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
