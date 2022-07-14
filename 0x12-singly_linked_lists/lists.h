c109e01551ef5ebd7f7614d0c0abf88847dd2067
Darren <darren2097@gmail.com>
2022-07-14 06:20:24 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct list_t
+ * @str: string
+ * @len: length of string
+ * @next: points to the next node
+ */
+
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
