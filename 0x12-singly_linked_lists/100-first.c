b117c53b875af5889f3a53f94f2a4d8043cf5049
Darren <darren2097@gmail.com>
2022-07-14 07:45:04 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+void first(void) __attribute__((constructor));
+
+/**
+ * first - prints before the main function
+ *
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
