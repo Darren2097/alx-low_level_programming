32adc0ba80eb5b1f55d0d3462a792883af9e84bd
Darren <darren2097@gmail.com>
2022-07-21 06:52:28 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,8 +11,8 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, power;
-	int length;
+	unsigned int num;
+	int length, i;
 
 	if (b == NULL)
 		return (0);
@@ -23,25 +23,20 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	length = 0;
-	while (*b != '\0')
+	while (b[length] != '\0')
 	{
 		length++;
 	}
 
 	num = 0;
-	power = 0;
-	while (length >= 0)
+	for (i = length - 1; i >= 0; i++)
 	{
-		if (*(b + length) == 1)
-		{
-			num += pow(2, power);
-		}
-		else if (*(b + length) != 0)
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
-		length--;
-		power++;
+		num += (b[i] - '0') * pow(2, length - i -1);
 	}
+
 	return (num);
 }
