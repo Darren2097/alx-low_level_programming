e99ad6f20f704e915292960e2551a8da3c2a49f8
Darren <darren2097@gmail.com>
2022-09-01 14:22:38 +0200

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,16 +23,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	while ((*h)->next != NULL)
 	{
 		length++;
 		*h = (*h)->next;
 	}
-
 	if (idx == length - 1)
-		return(add_dnodeint_end(h, n));
-
+		return (add_dnodeint_end(h, n));
 	if (*h == NULL)
 	{
 		new->prev = NULL;
