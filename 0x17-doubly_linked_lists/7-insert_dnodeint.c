0d730ea8266cf347e7da12d8015724d95ef62013
Darren <darren2097@gmail.com>
2022-09-01 14:39:30 +0200

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ size_t dlistint_len(const dlistint_t *h)
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	size_t length = 0;
+	size_t length;
 	unsigned int i = 0;
 	dlistint_t *new, *temp;
 
