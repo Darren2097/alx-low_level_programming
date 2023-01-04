84de2670969921444c78abbdf82b1f3a264ea4d8
Darren <darren2097@gmail.com>
2023-01-04 18:38:12 +0200

    task 103

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-void print_array(int *array, size_t m, size_t l);
+void print_array(int *array, size_t l, size_t r);
 int binary_search(int *array, size_t size, int value);
 int check_array(int *array, size_t size, size_t l, size_t r, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+void print_array(int *array, size_t f, size_t l);
+int binary_search(int *array, size_t size, int value, int f, int l);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
